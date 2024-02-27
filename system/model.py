f755d1df019be94de98fc5919e3cfe119ef05e3b
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-02-27 11:58:48 +0300

    database creation

--- system/model.py
+++ system/model.py
@@ -3,27 +3,41 @@ from system import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.string(50), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(20), unique=True, nullabe=False)
-    
+    password = db.Column(db.String(20), unique=True,  nullable=False)
+
+    def __repr__(self):
+        return f"[({self.first_name}), ({self.last_name}), ({self.last_email}), ({self.password})]"
+
+
 class Candidate(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(80), unique=True)
     phone = db.Column(db.String(16))
     candidate_img = db.Column(db.String(255), nullable=False, default="default.jpg")
     bio = db.Column(db.Text)
     position_id = db.Column(db.Integer, db.ForeignKey("position.id"))
-    
+
+    def __repr__(self):
+        return f"[({self.name}), ({self.emai}), ({self.phone}), ({self.candidte_img}), ({self.bio}), ({self.position_id})]"
+
+
 class Position(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     position_name = db.Column(db.String(40), unique=True)
-    
+
+    def __repr__(self):
+        return f"[({self.position_name})]"
+
+
 class BallotPosition(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     candidate_id = db.Column(db.Integer, db.ForeignKey('candidate.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     position_id = db.Column(db.Integer, db.ForeignKey('position.id'))
-    
+
+    def __repr__(self):
+        return f"[({self.candidate_id}), ({self.user_id}), ({self.position_id})]"
--- system/routes.py
+++ system/routes.py
@@ -1,6 +1,14 @@
-from flask import render_template, url_for
-from system import app
+from flask import redirect, render_template, url_for, flash
+from system import app, login_manager, db, bcrypt
+from flask_login import login_user, logout_user, current_user, login_required
 from system.form import LoginForm, RegistrationForm
+from system.model import User
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/')
 @app.route('/account/')
@@ -9,9 +17,25 @@ def login_home():
     form = LoginForm()
     return render_template('login.html', form=form)
 
-@app.route('/account/register')
+@app.route('/account/register' ,methods=['POST', 'GET'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('login'))
     form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = (bcrypt.generate_password_hash
+                           (form.password.data)
+                           .decode('utf-8'))
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created successfully!', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 @app.route('/ballot')
